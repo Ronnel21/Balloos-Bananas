d9204e7314acb915ad431d2f4638b0c7293d985e
Ronnel21 <ronnelian@yahoo.ca>
2018-02-26 00:56:48 -0500

    Enter

---
+++ Scripts/core/game.js
@@ -0,0 +1,21 @@
+//IIFE - Immediately Invoked Function Expression
+(function(){
+
+    function Init(){
+        console.log("Initializing Started.");
+    }
+
+    function Start(){
+
+    }
+
+    function Update(){
+
+    }
+
+    function Main(){
+         
+    }
+
+    window.onload = Init;
+})();
