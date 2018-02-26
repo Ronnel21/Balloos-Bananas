53f695cf8c7be48171e09abd8371c0cf4e25c180
Ronnel21 <ronnelian@yahoo.ca>
2018-02-26 01:18:57 -0500

    Typescript | CreateJS

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,31 +1,40 @@
 //IIFE - Immediately Invoked Function Expression
-(function () {
+(function(){
+
     //Game Variables
-    var canvas = document.getElementById("canvas");
-    var stage;
-    var helloLabel;
-    function Init() {
+    let canvas = document.getElementById("canvas");
+    let stage: createjs.Stage;
+    let helloLabel: createjs.Text;
+
+    function Init() : void{
         console.log("Initializing Started.");
+
         Start();
     }
-    function Start() {
+
+    function Start() : void {
         console.log("Starting Application.");
+
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60; // 60 Frames Per Second
         createjs.Ticker.on("tick", Update);
         Main();
     }
-    function Update() {
+
+    function Update() : void {
         helloLabel.rotation += 4;
         stage.update(); //Redraws the stage every frame
     }
-    function Main() {
+
+    function Main() : void {
         console.log("Game Started!");
+
         helloLabel = new createjs.Text("Hello Everyone!", "40px Times New Roman", "#234121");
         helloLabel.x = 100;
         helloLabel.y = 100;
+
         stage.addChild(helloLabel);
     }
+
     window.onload = Init;
 })();
-//# sourceMappingURL=game.js.map
